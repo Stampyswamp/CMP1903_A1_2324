455dfc57c73ea0db3dd03fb58463cf80d6a36a5e
Liam <158169933+Stampyswamp@users.noreply.github.com>
2024-03-05 15:22:38 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -5,7 +5,14 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         private int roll;
+        public int Roll //encapsulates value.
+        {
+            get { return roll; }
+            set { roll = value; }
+        }
+        
         private static Random rnd = new Random();
+        
         /// <summary>
         /// Produces a random roll 1-6. if (parameter is changed && and num > 6 is rolled) {debug class will flag this.} (not real code)
         /// </summary>
