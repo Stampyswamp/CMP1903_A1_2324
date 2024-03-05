8e54ef33bb88f8d0f1570fe0e929d68d93508547
Liam <158169933+Stampyswamp@users.noreply.github.com>
2024-03-05 10:52:03 +0000

    Update Program.cs
    Double Check.

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -6,27 +6,27 @@ namespace CMP1903_A1_2324
     {
         static void Main(string[] args)
         {
-            while (true)
+           while (true)
             { //loop forever unless N pressed
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("Roll Dice? (Y/N) ");
-                Console.ForegroundColor = ConsoleColor.White;
-                string RollAgain = Console.ReadLine();
-
-                //Switch is a way of taking in inputs and doing something with them. AKA guard clause (Making Sure the input is what you want, otherwise Handling It.)
-                switch (RollAgain)
-                {
-                    case "Y": ///If Y is Entered, the Object will be created and the Game will be Carried Out.
-                        Game game = new Game();
-                        game.Gameloop();
-                        continue;
-                    case "N": ///If N is Entered, The Object will not be created and the Program will Exit.
-                        break;
-                    default:
-                        Console.WriteLine("isn't Valid, please enter Y or N"); //If the wrong character is chosen, the game will not Play or Exit.
-                        continue;                                               //Simply make the user aware that this is not a valid response. 
-                }
-                break;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Roll Dice? (Y/N) ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string RollAgain = Console.ReadLine();
+        
+            //Switch is a way of taking in inputs and doing something with them. AKA guard clause (Making Sure the input is what you want, otherwise Handling It.)
+            switch (RollAgain)
+            {
+                case "Y": ///If Y is Entered, the Object will be created and the Game will be Carried Out.
+                    Game game = new Game();
+                    game.Gameloop();
+                    continue;
+                case "N": ///If N is Entered, The Object will not be created and the Program will Exit.
+                    break;
+                default:
+                    Console.WriteLine("isn't Valid, please enter Y or N"); //If the wrong character is chosen, the game will not Play or Exit.
+                    continue;                                               //Simply make the user aware that this is not a valid response. 
+            }
+            break;
             }
         }
     }
