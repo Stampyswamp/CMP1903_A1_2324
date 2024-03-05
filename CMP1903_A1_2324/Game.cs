bee842013eb391733c9580647c3125f5252e9b23
Liam <158169933+Stampyswamp@users.noreply.github.com>
2024-03-05 10:50:50 +0000

    Update Game.cs
    Tested code and fixed problems during runtime.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -7,30 +7,31 @@ namespace CMP1903_A1_2324
     {
         public void Gameloop()
         {
-
             //declare var
             var list = new List<int> { }; ///List is for calculating the sum.
-
-            private static Die die = new Die();
-            private static Testing debug = new Testing(); //Create Objects
-
+        
+            Testing debug = new Testing(); //Create Object
+        
             /*Rolls Dice
             Simple loop while roll the dice x amount of times, adding each roll to a list. */
-            
-            for (int i = 1; i < 4; i++)
+        
+            for (int i = 1; i< 4; i++)
             {
+                Die die = new Die();
                 int roll = die.Roll();
+                System.Threading.Thread.Sleep(roll);  //Creates Die Object. Rolls Dice. Waits 1-6ms.
+        
                 Console.WriteLine($"Dice " + i + ": " + roll); // Roll Dice and report Value
-                
-                debug.Assert(roll);
+        
+                debug.Assert(roll,3);
                 list.Add(roll); //check if the roll is the correct value, if so add to sum list. 
             }
-
+        
             /*Print Sum
             Each diceroll in the list will be added to the sum.*/
-            int sum = list.Sum(Convert.ToInt32());
+            int sum = list.Sum();
             Console.WriteLine($"\nSum: {sum}");
-            debug.Assert(sum);
+            debug.Assert(3,sum);
         }
     }
 }
