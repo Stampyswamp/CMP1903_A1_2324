4e800107b616af91712d4d78e583907afd25e0fa
Liam <158169933+Stampyswamp@users.noreply.github.com>
2024-03-05 10:29:38 +0000

    Update Testing.cs
    changed the method to use debug.assert

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -5,40 +5,17 @@ namespace CMP1903_A1_2324
     internal class Testing
     {
         /// <summary>
-        /// Checks if the roll is between 1-6. Changes Green or Red according to if the response is T/F.
+        /// Checks if the roll is between 1-6, or if the sum is between 3-18.
         /// </summary>
-        /// <param name="roll"></param>
+        /// <param name="roll", name="sum"></param>
         /// <returns> Writes in the console if the role is legal, else returns true/false incase you want to do something with this in the future. as no errors are raised. </returns>
-        public bool Assert(int roll)
+        public bool Assert(int roll = 3, int sum = 5)
         {
-            if (roll >= 1 && roll <= 6)
-            {
-                Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Roll is Legal."); Console.ForegroundColor = ConsoleColor.White;
-                return true;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Roll isn't Legal."); Console.ForegroundColor = ConsoleColor.White;
-                return false;
-            }
+            Debug.Assert(roll >= 1 && roll <= 6, "Number is not between 1 and 6")
+
+            Debug.Assert(sum >= 3 && sum <= 18, "sum is not between 3 and 18")
         }
-        /// <summary>
-        /// Checks if the sum is between 3-18 (1+1+1 or 6+6+6) (min/max). Changes Green or Red according to if the response is T/F.
-        /// </summary>
-        /// <param name="sum"></param> Sum is the total number after all 3 Dice rolls is added up. 
-        /// <returns> True or False </returns>
-        public bool AssertSum(int sum)
-        {
-            if (sum >= 3 && sum <= 18)
-            {
-                Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Sum is Legal. \n"); Console.ForegroundColor = ConsoleColor.White;
-                return true;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Sum isn't Legal. \n"); Console.ForegroundColor = ConsoleColor.White;
-                return false;
-            }
-        }
+      
+     
     }
 }
